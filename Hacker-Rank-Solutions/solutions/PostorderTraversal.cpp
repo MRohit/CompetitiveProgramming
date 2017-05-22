dbe50215ab22bf5f6b8a0153f9706acae841a2d3
Rohit <mourya.rohit32@gmail.com>
2017-05-22 18:58:09 +0530

    Postorder Traversal problem solution

---
+++ Hacker-Rank-Solutions/solutions/PostorderTraversal.cpp
@@ -0,0 +1,21 @@
+/* you only have to complete the function given below.  
+Node is defined as  
+
+struct node
+{
+    int data;
+    node* left;
+    node* right;
+};
+
+*/
+
+
+void postOrder(node *root) {
+if(root){
+        
+        postOrder(root->left);
+        postOrder(root->right);
+    cout<<root->data<<" ";
+    }
+}
