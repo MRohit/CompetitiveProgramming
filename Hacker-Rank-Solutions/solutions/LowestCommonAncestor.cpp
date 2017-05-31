cd9859c4872e61b3307a196e72fbd86c814cb90f
Rohit <mourya.rohit32@gmail.com>
2017-05-31 11:28:36 +0530

    Lowest Common Ancestor in BST

---
+++ Hacker-Rank-Solutions/solutions/LowestCommonAncestor.cpp
@@ -0,0 +1,28 @@
+/*
+Node is defined as 
+
+typedef struct node
+{
+   int data;
+   node * left;
+   node * right;
+}node;
+
+*/
+
+
+node * lca(node * root, int v1,int v2)
+{
+    // base condition
+    if(root==NULL)  return NULL;
+    
+    // if node is less than v1 and v2 then LCA lies at right part
+    if(root->data < v1 && root->data < v2)
+        return lca(root->right,v1,v2);
+    // if node is greater than v1 and v2 then LCA lies at left part
+    if(root->data > v1 && root->data > v2)
+        return lca(root->left,v1,v2);
+    return root;
+   
+}
+
