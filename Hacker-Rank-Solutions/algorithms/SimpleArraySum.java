6a2bda48496c35f4d2fe914adf6de2c52518c405
Rohit <mourya.rohit32@gmail.com>
2017-07-04 23:22:18 +0530

    Simple array solutions

---
+++ Hacker-Rank-Solutions/algorithms/SimpleArraySum.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int sum = 0;
+        int arr[] = new int[n];
+        for(int arr_i=0; arr_i < n; arr_i++){
+            arr[arr_i] = in.nextInt();
+        }
+        for(int arr_i=0; arr_i < n; arr_i++){
+            sum = sum + arr[arr_i];
+        }
+        System.out.println(sum);
+    }
+}
