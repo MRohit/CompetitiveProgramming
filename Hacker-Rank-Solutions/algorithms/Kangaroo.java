7c5ece95ac18cc1ed330595beebbbd13e8925799
Rohit <mourya.rohit32@gmail.com>
2017-07-19 15:42:48 +0530

    Kangaroo problem solution

---
+++ Hacker-Rank-Solutions/algorithms/Kangaroo.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static String kangaroo(int x1, int v1, int x2, int v2) {
+        // Complete this function
+		if(v1-v2 !=0){
+	        int r=(x2-x1)%(v1-v2);
+	        if(v1 > v2 && r==0)
+	            return "YES";
+	        else
+	            return "NO";
+		}
+		return "NO";
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x1 = in.nextInt();
+        int v1 = in.nextInt();
+        int x2 = in.nextInt();
+        int v2 = in.nextInt();
+        String result = kangaroo(x1, v1, x2, v2);
+        System.out.println(result);
+    }
+}
