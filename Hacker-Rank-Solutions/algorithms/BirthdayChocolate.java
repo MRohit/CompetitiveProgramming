c736962edc96bffa1fe9b4a5a86e9dc04033223e
Rohit <mourya.rohit32@gmail.com>
2017-07-21 13:20:59 +0530

    Birthday Chocolate problem solution

---
+++ Hacker-Rank-Solutions/algorithms/BirthdayChocolate.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int solve(int n, int[] s, int d, int m){
+        int cnt=0;
+        for(int i=0;i<=n-m;++i) {
+            int sum = s[i];
+            for(int j=1;j<m;++j){
+                sum = sum + s[i+j];
+            }
+            if(sum == d)
+                cnt++;
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] s = new int[n];
+        for(int s_i=0; s_i < n; s_i++){
+            s[s_i] = in.nextInt();
+        }
+        int d = in.nextInt();
+        int m = in.nextInt();
+        int result = solve(n, s, d, m);
+        System.out.println(result);
+    }
+}
