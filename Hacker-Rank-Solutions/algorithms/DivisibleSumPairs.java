bad515800f9b0dec3cd007f2449de07dd10615c3
Rohit <mourya.rohit32@gmail.com>
2017-07-23 17:20:27 +0530

    divisible Sum Pairs problem solution

---
+++ Hacker-Rank-Solutions/algorithms/DivisibleSumPairs.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int divisibleSumPairs(int n, int k, int[] ar) {
+        // Complete this function
+        int cnt=0;
+        for(int i=0;i<n;++i) {
+            for(int j=i+1;j<n;++j) {
+                if((ar[i]+ar[j])%k == 0)
+                    cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = divisibleSumPairs(n, k, ar);
+        System.out.println(result);
+    }
+}
