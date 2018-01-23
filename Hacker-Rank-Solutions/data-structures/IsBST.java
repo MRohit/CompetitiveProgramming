efc2f68736e0074860ab853d94e0c5dc2e98e875
Rohit <mourya.rohit32@gmail.com>
2018-01-24 00:21:02 +0530

    Solving using inorder traversal

--- Hacker-Rank-Solutions/data-structures/IsBST.java
+++ Hacker-Rank-Solutions/data-structures/IsBST.java
@@ -5,36 +5,21 @@ The Node class is defined as follows:
         int data;
         Node left;
         Node right;
+        Node prev;
      }
 */
-int findMin (Node root) {
-    if (root == null)
-        return Integer.MIN_VALUE;
-    while (root.left != null)
-        root = root.left;
-    return root.data;
-}
-int findMax (Node root) {
-    if (root == null)
-        return Integer.MAX_VALUE;
-    while (root.right != null)
-        root = root.right;
-    return root.data;
-}
-    boolean checkBST(Node root) {
-        // base case
-        if (root == null)
-            return true;
-        // check if left is greater, if yes return false
-        if (root.left != null && root.left.data < findMax (root.left))
-            return false;
-        // check if right is smaller, if yes return false
-        if (root.right != null && root.right.data > findMin(root.right))
-            return false;
-        // recursively check if left and right is not a BST
-        if (!checkBST (root.left) || !checkBST (root.right))
-            return false;
-
-        // if above all are true then return true;
+    boolean isBST (Node root, Node prev) {
+        if (root != null) {
+            if (!isBST(root.left,prev))
+                return false;
+            if (prev != null && root.data <= prev.data)
+                return false;
+            prev = root;
+            return isBST(root.right,prev);
+        }
         return true;
+    }
+    boolean checkBST(Node root) {
+        Node prev = null;
+        return isBST(root, prev);
     }
